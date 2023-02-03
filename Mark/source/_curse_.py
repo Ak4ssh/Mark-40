4d8817d95a7640f7c379890ef690852ce84c3e31
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2023-02-03 13:55:44 +0530

    Create _curse_.py

---
+++ Mark/source/_curse_.py
@@ -0,0 +1,296 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from telegram import update 
+
+from .. import (Mark, Session, Session2, Session3, Session4,
+               Session5, Session6, Session7, Session8, Session9, Session10, Session11,
+               Session12, Session13, Session14, Session15, Session16, Session17, Session18,
+               Session19, Session20, CURSE_HNDLR)
+x = True
+@Mark.on_message(filters.command(["urse"], prefixes=CURSE_HNDLR))
+async def oneword(event):
+    while x != False:
+        message = update.effective_message  
+        await event.delete()
+        kek = event.chat_id
+        if message.reply_to_message:
+            if Session:
+                await Session.message.reply_to_message.reply_text(kek, "AJA")
+            if Session2:
+                await Session2.message.reply_to_message.reply_text(kek, "CHAL")
+            if Session3:
+                await Session3.message.reply_to_message.reply_text(kek, "RHA")
+            if Session4:
+                await Session4.message.reply_to_message.reply_text(kek, "TERA")
+            if Session5:
+                await Session5.message.reply_to_message.reply_text(kek, "KEYBOARD")
+            if Session6:
+                await Session6.message.reply_to_message.reply_text(kek, "RANDI")
+            if Session7:
+                await Session7.message.reply_to_message.reply_text(kek, "SUAR")
+            if Session8:
+                await Session8.message.reply_to_message.reply_text(kek, "CHMAR")
+            if Session9:
+                await Session9.message.reply_to_message.reply_text(kek, "BSDK")
+            if Session10:
+                await Session10.message.reply_to_message.reply_text(kek, "TERI")
+            if Session11:
+                await Session11.message.reply_to_message.reply_text(kek, "MAAA")
+            if Session12:
+                await Session12.message.reply_to_message.reply_text(kek, "KA")
+            if Session13:
+                await Session13.message.reply_to_message.reply_text(kek, "BHOSDA")
+            if Session14:
+                await Session14.message.reply_to_message.reply_text(kek, "BEHEN")
+            if Session15:
+                await Session15.message.reply_to_message.reply_text(kek, "KE")
+            if Session16:
+                await Session16.message.reply_to_message.reply_text(kek, "LAWDE")
+            if Session17:
+                await Session17.message.reply_to_message.reply_text(kek, "RANDI")
+            if Session18:
+                await Session18.message.reply_to_message.reply_text(kek, "KI")
+            if Session19:
+                await Session19.message.reply_to_message.reply_text(kek, "AULAAD")
+            if Session20:
+                await Session20.message.reply_to_message.reply_text(kek, "TERI")
+            if Session:
+                await Session.message.reply_to_message.reply_text(kek, "MAA")
+            if Session2:
+                await Session2.message.reply_to_message.reply_text(kek, "KI")
+            if Session3:
+                await Session3.message.reply_to_message.reply_text(kek, "CHUTT")
+            if Session4:
+                await Session4.message.reply_to_message.reply_text(kek, "MAAAAIIII")
+            if Session5:
+                await Session5.message.reply_to_message.reply_text(kek, "MERAAA")
+            if Session6:
+                await Session6.message.reply_to_message.reply_text(kek, "LOOOODDDAAA")
+            if Session7:
+                await Session7.message.reply_to_message.reply_text(kek, "BHENNN")
+            if Session8:
+                await Session8.message.reply_to_message.reply_text(kek, "KEE")
+            if Session9:
+                await Session9.message.reply_to_message.reply_text(kek, "BHOSDEE")
+            if Session10:
+                await Session10.message.reply_to_message.reply_text(kek, "KKOOOO")
+            if Session11:
+                await Session11.message.reply_to_message.reply_text(kek, "APNEE")
+            if Session12:
+                await Session12.message.reply_to_message.reply_text(kek, "LUND")
+            if Session13:
+                await Session13.message.reply_to_message.reply_text(kek, "SEE")
+            if Session14:
+                await Session14.message.reply_to_message.reply_text(kek, "CHODDUU")
+            if Session15:
+                await Session15.message.reply_to_message.reply_text(kek, "AAJAA")
+            if Session16:
+                await Session16.message.reply_to_message.reply_text(kek, "BETEE")
+            if Session17:
+                await Session17.message.reply_to_message.reply_text(kek, "BAHENN")
+            if Session18:
+                await Session18.message.reply_to_message.reply_text(kek, "KEE")
+            if Session19:
+                await Session19.message.reply_to_message.reply_text(kek, "LODOD")
+            if Session20:
+                await Session20.message.reply_to_message.reply_text(kek, "ABB")
+            if Session:
+                await Session.message.reply_to_message.reply_text(kek, "KAROO")
+            if Session2:
+                await Session2.message.reply_to_message.reply_text(kek, "SPEED")
+            if Session3:
+                await Session3.message.reply_to_message.reply_text(kek, "BEHEN")
+            if Session4:
+                await Session4.message.reply_to_message.reply_text(kek, "KA")
+            if Session5:
+                await Session5.message.reply_to_message.reply_text(kek, "LAWDA")
+            if Session6:
+                await Session6.message.reply_to_message.reply_text(kek, "RANDI")
+            if Session7:
+                await Session7.message.reply_to_message.reply_text(kek, "KI")
+            if Session8:
+                await Session8.message.reply_to_message.reply_text(kek, "AULLAADD")
+            if Session9:
+                await Session9.message.reply_to_message.reply_text(kek, "teri")
+            if Session10:
+                await Session10.message.reply_to_message.reply_text(kek, "MAA")
+            if Session11:
+                await Session11.message.reply_to_message.reply_text(kek, "KI")
+            if Session12:
+                await Session12.message.reply_to_message.reply_text(kek, "CHUT ME MERA LODA")
+            if Session13:
+                await Session13.message.reply_to_message.reply_text(kek, "RANDI")
+            if Session14:
+                await Session14.message.reply_to_message.reply_text(kek, "KYA")
+            if Session15:
+                await Session15.message.reply_to_message.reply_text(kek, "HUA")
+            if Session16:
+                await Session16.message.reply_to_message.reply_text(kek, "BAAP")
+            if Session17:
+                await Session17.message.reply_to_message.reply_text(kek, "KE")
+            if Session18:
+                await Session18.message.reply_to_message.reply_text(kek, "AAGE")
+            if Session19:
+                await Session19.message.reply_to_message.reply_text(kek, "NHI")
+            if Session20:
+                await Session20.message.reply_to_message.reply_text(kek, "CHAL")
+            if Session:
+                await Session.message.reply_to_message.reply_text(kek, "RHA")
+            if Session2:
+                await Session2.message.reply_to_message.reply_text(kek, "TERII")
+            if Session3:
+                await Session3.message.reply_to_message.reply_text(kek, "MAA KOO")
+            if Session4:
+                await Session4.message.reply_to_message.reply_text(kek, "NILAM KRDU")
+            if Session5:
+                await Session5.message.reply_to_message.reply_text(kek, "BEHENN KE LUNDD")
+            if Session6:
+                await Session6.message.reply_to_message.reply_text(kek, "SUWARR")
+            if Session7:
+                await Session7.message.reply_to_message.reply_text(kek, "GAWARR")
+            if Session8:
+                await Session8.message.reply_to_message.reply_text(kek, "KEEE")
+            if Session9:
+                await Session9.message.reply_to_message.reply_text(kek, "CHODEEE")
+            if Session10:
+                await Session10.message.reply_to_message.reply_text(kek, "TERII")
+            if Session11:
+                await Session11.message.reply_to_message.reply_text(kek, "MUJRAA")
+            if Session12:
+                await Session12.message.reply_to_message.reply_text(kek, "KREE")
+            if Session13:
+                await Session13.message.reply_to_message.reply_text(kek, "WALIII")
+            if Session14:
+                await Session14.message.reply_to_message.reply_text(kek, "CHODUUUU")
+            if Session15:
+                await Session15.message.reply_to_message.reply_text(kek, "TERII")
+            if Session16:
+                await Session16.message.reply_to_message.reply_text(kek, "TERII")
+            if Session17:
+                await Session17.message.reply_to_message.reply_text(kek, "MA KAAA")
+            if Session18:
+                await Session18.message.reply_to_message.reply_text(kek, "GANG RAPE")
+            if Session19:
+                await Session19.message.reply_to_message.reply_text(kek, "KARDUGAA")
+            if Session20:
+                await Session20.message.reply_to_message.reply_text(kek, "TEREE")
+            if Session:
+                await Session.message.reply_to_message.reply_text(kek, "VAISHYAA")
+            if Session2:
+                await Session2.message.reply_to_message.reply_text(kek, "BEHENN KEEE")
+            if Session3:
+                await Session3.message.reply_to_message.reply_text(kek, "GANDEE BHOSDEE")
+            if Session4:
+                await Session4.message.reply_to_message.reply_text(kek, "PAR APNA")
+            if Session5:
+                await Session5.message.reply_to_message.reply_text(kek, "RAKH KE")
+            if Session6:
+                await Session6.message.reply_to_message.reply_text(kek, "PREGNENNT KRUU")
+            if Session7:
+                await Session7.message.reply_to_message.reply_text(kek, "MAAII")
+            if Session8:
+                await Session8.message.reply_to_message.reply_text(kek, "TERAA")
+            if Session9:
+                await Session9.message.reply_to_message.reply_text(kek, "BBAAAP")
+            if Session10:
+                await Session10.message.reply_to_message.reply_text(kek, "HHUUU")
+            if Session11:
+                await Session11.message.reply_to_message.reply_text(kek, "SMJHAA BSDKK")
+            if Session12:
+                await Session12.message.reply_to_message.reply_text(kek, "TOR")
+            if Session13:
+                await Session13.message.reply_to_message.reply_text(kek, "BBUUR")
+            if Session14:
+                await Session14.message.reply_to_message.reply_text(kek, "CHODAA")
+            if Session15:
+                await Session15.message.reply_to_message.reply_text(kek, "RANDIII")
+            if Session16:
+                await Session16.message.reply_to_message.reply_text(kek, "KKEEE")
+            if Session17:
+                await Session17.message.reply_to_message.reply_text(kek, "BEEJ")
+            if Session18:
+                await Session18.message.reply_to_message.reply_text(kek, "KEE")
+            if Session19:
+                await Session19.message.reply_to_message.reply_text(kek, "BHOSDEE")
+            if Session20:
+                await Session20.message.reply_to_message.reply_text(kek, "TERIII")
+            if Session:
+                await Session.message.reply_to_message.reply_text(kek, "MMAAA")
+            if Session2:
+                await Session2.message.reply_to_message.reply_text(kek, "NNAA")
+            if Session3:
+                await Session3.message.reply_to_message.reply_text(kek, "MARJAYEE")
+            if Session4:
+                await Session4.message.reply_to_message.reply_text(kek, "TEEERRII")
+            if Session5:
+                await Session5.message.reply_to_message.reply_text(kek, "MAA")
+            if Session6:
+                await Session6.message.reply_to_message.reply_text(kek, "KKOO")
+            if Session7:
+                await Session7.message.reply_to_message.reply_text(kek, "KUTTO")
+            if Session8:
+                await Session8.message.reply_to_message.reply_text(kek, "KEEE")
+            if Session9:
+                await Session9.message.reply_to_message.reply_text(kek, "MUH")
+            if Session10:
+                await Session10.message.reply_to_message.reply_text(kek, "MUH")
+            if Session11:
+                await Session11.message.reply_to_message.reply_text(kek, "MMAAII")
+            if Session12:
+                await Session12.message.reply_to_message.reply_text(kek, "DALDUNGA")
+            if Session13:
+                await Session13.message.reply_to_message.reply_text(kek, "BHEN KE LAWDDE")
+            if Session14:
+                await Session14.message.reply_to_message.reply_text(kek, "SALE")
+            if Session15:
+                await Session15.message.reply_to_message.reply_text(kek, "EEK")
+            if Session16:
+                await Session16.message.reply_to_message.reply_text(kek, "RAPTE")
+            if Session17:
+                await Session17.message.reply_to_message.reply_text(kek, "MEE")
+            if Session18:
+                await Session18.message.reply_to_message.reply_text(kek, "TEERRII")
+            if Session19:
+                await Session19.message.reply_to_message.reply_text(kek, "BHEN")
+            if Session20:
+                await Session20.message.reply_to_message.reply_text(kek, "MERA")
+            if Session:
+                await Session.message.reply_to_message.reply_text(kek, "LUUNNDD")
+            if Session2:
+                await Session2.message.reply_to_message.reply_text(kek, "CHAHTTII")
+            if Session3:
+                await Session3.message.reply_to_message.reply_text(kek, "FIREGI")
+            if Session4:
+                await Session4.message.reply_to_message.reply_text(kek, "PAPA HAI HUM")
+            if Session5:
+                await Session5.message.reply_to_message.reply_text(kek, "TERE")
+            if Session6:
+                await Session6.message.reply_to_message.reply_text(kek, "BAAP")
+            if Session7:
+                await Session7.message.reply_to_message.reply_text(kek, "SE")
+            if Session8:
+                await Session8.message.reply_to_message.reply_text(kek, "BAKCHODI")
+            if Session9:
+                await Session9.message.reply_to_message.reply_text(kek, "KARNE")
+            if Session10:
+                await Session10.message.reply_to_message.reply_text(kek, "AYEGA")
+            if Session11:
+                await Session11.message.reply_to_message.reply_text(kek, "TOO")
+            if Session12:
+                await Session12.message.reply_to_message.reply_text(kek, "GAND")
+            if Session13:
+                await Session13.message.reply_to_message.reply_text(kek, "MAAII")
+            if Session14:
+                await Session14.message.reply_to_message.reply_text(kek, "SARIYA")
+            if Session15:
+                await Session15.message.reply_to_message.reply_text(kek, "DAL")
+            if Session16:
+                await Session16.message.reply_to_message.reply_text(kek, "DALDUNGGAA")
+            if Session17:
+                await Session17.message.reply_to_message.reply_text(kek, "BETEE")
+            if Session18:
+                await Session18.message.reply_to_message.reply_text(kek, "JJAKE")
+            if Session19:
+                await Session19.message.reply_to_message.reply_text(kek, "APNNI")
+            if Session20:
+                await Session20.message.reply_to_message.reply_text(kek, "MUMMY CHUDA")
